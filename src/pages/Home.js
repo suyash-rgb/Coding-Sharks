94553ec7747af21a131d2baf5f02faa03b089592
Suyash Baoney <suyashbaoney58@gmail.com>
2026-01-02 23:56:08 +0530

    fixed navbar width and scroll margin top issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,9 +12,35 @@ function Home() {
       <div className="Home">
         <Navbar />
         <img src={front} alt="JavaScript concepts" className="image" />
-        {/* <br></br> <br></br> */}
-        <h1 className="heading">Javascript programming examples, exercises and solutions for beginners</h1>
-        <div className="cards-container" id="learning-paths">
+        
+        <div style={{ padding: "40px 20px", textAlign: "center", maxWidth: "1200px", margin: "0 auto" }}>
+          <h1 className="heading">Javascript Programming Examples, Exercises and Solutions for Beginners</h1>
+          <p style={{ 
+            fontSize: "1.1rem", 
+            color: "#666", 
+            lineHeight: "1.8", 
+            marginBottom: "40px",
+            maxWidth: "800px",
+            margin: "0 auto 40px"
+          }}>
+            Welcome to JS Mentor! Master JavaScript through hands-on learning. Explore comprehensive tutorials, practical exercises, and real-world examples designed to help you become a confident JavaScript developer. Start your coding journey today.
+          </p>
+        </div>
+
+        <h2 
+        id="learning-paths" 
+        style={{ 
+          textAlign: "center", 
+          color: "rgb(240, 82, 4)", 
+          fontSize: "2rem", 
+          marginBottom: "30px",
+          fontWeight: "600",
+          scrollMarginTop: "140px"
+        }}>
+          Explore Learning Paths
+        </h2>
+        
+        <div className="cards-container">
           {programmingData.cards.map((card, index) => (
             <Card
               key={index}
